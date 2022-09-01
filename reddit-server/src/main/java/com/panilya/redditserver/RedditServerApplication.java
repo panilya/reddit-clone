1568b6369a1c3d66648bd4b849d41711503324e3
Ilya Pantsyr <panilyau@gmail.com>
2022-09-01 20:06:09 +0300

    Project initialization

---
+++ reddit-server/src/main/java/com/panilya/redditserver/RedditServerApplication.java
@@ -0,0 +1,13 @@
+package com.panilya.redditserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditServerApplication.class, args);
+    }
+
+}
---
+++ reddit-server/src/test/java/com/panilya/redditserver/RedditServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panilya.redditserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
